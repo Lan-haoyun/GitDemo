96688f97dd4e47224b690424f5dd130f9acceb6e
Lan-haoyun <1654315871@qq.com>
2018-06-04 18:49:44 +0800

    创建

---
+++ app/src/main/java/com/bw/gitdemo/Ab.java
@@ -0,0 +1,9 @@
+package com.bw.gitdemo;
+
+/**
+ * Created by 兰昊琼 on 2018/6/4.
+ */
+
+public class Ab {
+
+}
--- app/src/main/java/com/bw/gitdemo/MainActivity.java
+++ app/src/main/java/com/bw/gitdemo/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         initView();
         mTv.setText("git");
 
+
     }
 
     private void initView() {
